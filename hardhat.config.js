b3f3b22d1fb2e71ba119d046f869c2eb3f848935
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2023-07-19 14:36:42 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   networks: {
     goerli: {
       url: "https://goerli.infura.io/v3/336a201c36684a2f884db2578ac19cff",
-      accounts: ['24cd23a482e9ab5ec92e505539114b9bfb42aff73dde59512d095502d78cdaaa'],
+      accounts: ['YOUR_PRIVATE_KEY'],
     },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com/",
-      accounts: ['24cd23a482e9ab5ec92e505539114b9bfb42aff73dde59512d095502d78cdaaa'],
+      accounts: ['YOUR_PRIVATE_KEY'],
     },
   },
 };
